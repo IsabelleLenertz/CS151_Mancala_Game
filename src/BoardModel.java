c173ce22ed072456c4d57f4cc8b098bfb32e41ee
Lucas <lucasgsc188@gmail.com>
2018-04-16 20:07:26 -0700

    Create BoardModel class with some starter code
    The BoardModel class was created with some code inside of it. Currently it still needs to have the play method fully written and the notify view method needs to be created.

---
+++ src/BoardModel.java
@@ -0,0 +1,36 @@
+/**
+ * Represents the model object which stores
+ * references to all high level data structures used to
+ * play the game of mancala.
+ * 
+ * @author Lucas Galleguillos
+ */
+public class BoardModel {
+	private BoardView view;
+	private Hole[] holes;
+	private Player[] players;
+	
+	/**
+	 * Default ctor for the class. Takes a single argument
+	 * that changes what the style of the final display will
+	 * be for the user.
+	 * 
+	 * @param view BoardView instance that determines style of board for user
+	 */
+	public BoardModel(BoardView view) {
+		this.view = view;
+		holes = new Hole[14];
+		players = new Player[2];
+		
+		players[0] = new Player(); // Player 1
+		players[1] = new Player(); // Player 2
+	}
+	
+	/**
+	 * Method which contains the logic for the game. Initiates a new game
+	 * when called.
+	 */
+	public void play() {
+		
+	}
+}
