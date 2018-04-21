9d254969c115f5599a4f7f0561af64a026529e83
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-04-21 08:18:41 -0700

    Player implemented, not tested

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Part of a group project
  * Player in a mancala game
@@ -31,7 +33,7 @@ public class Player {
 	 * @return current score
 	 */
 	public int getScore() {
-		return 0;
+		return mancala.getStones();
 	}
 	
 	/**
@@ -58,12 +60,44 @@ public class Player {
 	
 	/**
 	 * play the player's turn with a specific hole number
-	 * @param holeNumber hole selected by the player
+	 * @param holeIndex hole selected by the player
 	 * @return current score
 	 * precondition: the hole has to be on the player's side: up to the caller to check
 	 * postcondition: modifies the content of the holes, up to the caller to notify the views of the change
 	 */
-	public int play(int holeNumber) {
-		return 0;
+	public int play(int holeIndex) {
+		
+		int stonesCollected = holes[holeIndex].removeAllStones();
+		
+		// Distribute the collected stones in the holes
+		while(stonesCollected > 0) {
+			// Check if the hole is not the other player's mancala
+			if(holes[holeIndex].getClass() == mancala.getClass()) {
+				if(holes[holeIndex] != mancala) {
+					holeIndex++;
+				}
+			}
+			// Add a stone and move on to the next hole
+			holes[holeIndex].incrementStones();
+			holeIndex++;
+			stonesCollected--;
+		}
+		holeIndex--;
+		
+		// If the last hole was one of the player's pit
+		if (Arrays.asList(pits).contains(holes[holeIndex])) {
+			if (holes[holeIndex].getStones() == 1) {
+				holes[holeIndex].decrementStones();
+				stonesCollected = holes[(holeIndex + 7)%14].removeAllStones() + 1;
+				mancala.addStones(stonesCollected);
+			}
+		}
+		
+		// If the last hole was not the player's mancala, end the turn
+		if(holes[holeIndex] != mancala) {
+			endTurn();
+		}
+			
+		return getScore();
 	}
 }
