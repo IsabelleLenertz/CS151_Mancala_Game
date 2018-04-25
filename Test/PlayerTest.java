2f91073df584330c5e6fdb2c7af77700cf2fb648
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-04-25 11:35:23 -0700

    BoardStrategy interface implemented with JavaDoc

--- Test/PlayerTest.java
+++ Test/PlayerTest.java
@@ -1,9 +1,9 @@
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Tester for the Player class
--- src/BoardStrategy.java
+++ src/BoardStrategy.java
@@ -1,12 +1,65 @@
+import java.awt.Graphics2D;
 
 /**
  * Interface for the Strategy in the Strategy pattern
  * Used by the BoardView to define the shape and colors of the board
  * @author Isabelle Delmas
- * @date 
+ * @date 05-25-2018
  * @revision				Reason:
  *
  */
 public interface BoardStrategy {
-	//TODO
+	/**
+	 * Draw a pit on the board view
+	 * @param xPosition x position of the pit (upper left corner)
+	 * @param yPosition y position of the pit (upper left corner)
+	 * @param height height of the pit
+	 * @param width width of the pit
+	 * @param g2 graphical context
+	 */
+	public void drawPit(int xPosition, int yPosition, int height, int width, Graphics2D g2);
+	
+	/**
+	 * Draw a mancala on the board view
+	 * @param xPosition x position of the mancala (upper left corner)
+	 * @param yPosition y position of the mancala (upper left corner)
+	 * @param height height of the pit
+	 * @param width width of the pit
+	 * @param g2 graphical context
+	 */
+	public void drawMancala(int xPosition, int yPosition, int height, int width, Graphics2D g2);
+	
+	/**
+	 * Display the score of the player on the board view
+	 * @param score score to display
+	 * @param xPosition x position of the score (upper left corner)
+	 * @param yPosition y position of the score (upper left corner)
+	 * @param g2 graphical context
+	 */
+	public void displayScrore(int score, int xPosition, int yPosition, Graphics2D g2);
+	
+	/**
+	 * Display the name of the player on the board view
+	 * @param playerName name of the player
+	 * @param xPosition x position of the player (upper left corner)
+	 * @param yPosition y position of the player (upper left corner)
+	 * @param g2 graphical context
+	 */
+	public void displayPlayer(String playerName, int xPosition, int yPosition, Graphics2D g2);
+	
+	/**
+	 * Display the boarder of the board on the board view
+	 * @param height height of the board view
+	 * @param width width of the board view
+	 * @param g2 graphical context
+	 */
+	public void drawBoardBoarder( int height, int width, Graphics2D g2);
+	
+	/**
+	 * Display the background of the board inside the board view
+	 * @param height height of the board view
+	 * @param width width of the board view
+	 * @param g2 graphical context
+	 */
+	public void drawBoardFillIn( int height, int width, Graphics2D g2);
 }
