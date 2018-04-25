1831096479f1ab7e271c36a513bd4a7b7f08d463
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-04-25 11:36:38 -0700

    stub added to SquareBoard and RoundBoard so they implement the interface

--- src/RoundBoard.java
+++ src/RoundBoard.java
@@ -1,4 +1,41 @@
+import java.awt.Graphics2D;
 
 public class RoundBoard implements BoardStrategy {
+
+	@Override
+	public void drawPit(int xPosition, int yPosition, int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawMancala(int xPosition, int yPosition, int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayScrore(int score, int xPosition, int yPosition, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayPlayer(String playerName, int xPosition, int yPosition, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawBoardBoarder(int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawBoardFillIn(int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
 	//TODO
 }
--- src/SquareBoard.java
+++ src/SquareBoard.java
@@ -1,7 +1,45 @@
+import java.awt.Graphics2D;
+
 /**
  * @author isabelle
  *
  */
 public class SquareBoard implements BoardStrategy {
+
+	@Override
+	public void drawPit(int xPosition, int yPosition, int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawMancala(int xPosition, int yPosition, int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayScrore(int score, int xPosition, int yPosition, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void displayPlayer(String playerName, int xPosition, int yPosition, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawBoardBoarder(int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drawBoardFillIn(int height, int width, Graphics2D g2) {
+		// TODO Auto-generated method stub
+		
+	}
 	//TODO
 }
