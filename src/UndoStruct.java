03ff220ab046cb37b060440660b94f9d43bf496a
Yilin Zhao <josephzhao15@gmail.com>
2018-04-27 13:07:45 -0700

    implemented undoStruct class

--- src/UndoStruct.java
+++ src/UndoStruct.java
@@ -1,4 +1,48 @@
-
+/**
+ * @author Yilin Zhao
+ * @date 04/27/18
+ * holds the most recent pits for undo
+ *
+ */
 public class UndoStruct {
-	//TODO
+	int[] holes;
+    Player whoseTurn;
+    public UndoStruct(){
+        holes = new int[13];
+
+    }
+
+    /**
+     *
+     * @param newHoles the most recent set of holes
+     */
+    public void setHoles(int[] newHoles){
+        holes = newHoles;
+
+    }
+
+    /**
+     *
+     * @return return most recent set of holes
+     */
+    public int[] getHoles(){
+        return holes;
+    }
+
+    /**
+     *
+     * @param p player that's taking turn
+     */
+    public void setWhoseTurn(Player p){
+        whoseTurn = p;
+    }
+
+    /**
+     *
+     * @return player that's taking turn
+     */
+
+    public Player getWhoseTurn() {
+        return whoseTurn;
+    }
 }
