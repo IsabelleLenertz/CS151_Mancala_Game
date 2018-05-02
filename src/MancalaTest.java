476646b057340a9b409b2cde9e2703e92120d2c0
Yilin Zhao <josephzhao15@gmail.com>
2018-05-02 16:57:59 -0700

    update mancalaTest

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -7,8 +7,14 @@
 public class MancalaTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int stones;
+		BoardStrategy selectedStragegy;
+		BoardModel boardModel = new BoardModel();
+		BoardView boardView = new BoardView(1000);
+		boardView.setBoardModel(boardModel);
+		boardModel.setBoardView(boardView);
+		StartWindow sw = new StartWindow();
+		
 	}
 
 }
